2d6ec65467f1c7fdd59d23a458644e4af699f272
Jason <jason@solidterrace.com>
2016-12-14 02:37:57 -0500

    Updated Ignored

--- PTMSMirthInterface/PTMS.HttpsAuthProxy/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PTMS.HttpsAuthProxy.WebApiApplication" Language="C#" %>
--- SQL/Update Proxy.sql
+++
@@ -1,9 +0,0 @@
-select * from AspNetUserLogins
-
-select * from AspNetUserClaims
-
-select * from AspNetUsers
-
-
-
--- update aspnetusers set PracticeId = '0ed6c1a8-7af9-4578-a5e6-91eb26406a14' where Id = '148f746d-115a-4752-ae55-64e7c7429d76'
